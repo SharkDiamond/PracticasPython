13142774469d99afb661b2ce797b06f4a29e52c4
ComputadoraDeMesa Gabriel <gabrielarispe24@gmail.com>
2021-01-07 09:32:22 -0430

    cambios de pruebas

--- Poo.py
+++ Poo.py
@@ -5,3 +5,14 @@ class Carro():
     Color="naranja"
     Marca=""
     Encendido=False
+
+
+    def cambiarEstado(self):
+        
+        if self.Encendido:
+            
+            self.Encendido=False
+
+        else:
+        
+            self.Encendido=True    
