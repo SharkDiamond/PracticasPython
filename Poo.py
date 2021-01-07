0de93eb0ce52e5f5e9cd7dab2a236d5820064da7
ComputadoraDeMesa Gabriel <gabrielarispe24@gmail.com>
2021-01-07 09:27:02 -0430

    cambios de prueba

--- Poo.py
+++ Poo.py
@@ -1,39 +1,7 @@
 #Importaciones
 
-
-
 class Carro():
-	
-	#Propiedades	
-	color="naraja"
-	Modelo="Ferrari"
-	largo="2 Metros"
-	Cauchos="Pirelli"
-    encendido=False
-
-
-	#Comportamiento
-    def EncenderOapagar(self):
-
-    	if self.encendido:
-    		
-    		return self.encendido=False
-		    print("auto apagado")	
-    	
-    	else:
-    	
-    		return self.encendido=True	
-    		print("auto encendido")
-
-
-
-Focus=Carro();
-
-
-Focus.EncenderOapagar();
-
-print(Focus.color)
-
-
-print(Focus.color="Naranja")
-	
+    
+    Color="naranja"
+    Marca=""
+    Encendido=False
